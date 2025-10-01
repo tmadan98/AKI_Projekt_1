3230ad55b50d89239660c5747a4edd848f7a781e
Killerboriz <borizjohn7@gmail.com>
2025-10-01 20:46:12 +0200

    Cooles Ding
    Ich arbeite gerade dran

---
+++ PasswordGenerator.py
@@ -0,0 +1,32 @@
+import random
+from wypp import *
+import string
+
+def generatePassword(length: int):
+
+    pwsd = ""
+
+    lowercase = list(string.ascii_lowercase)   
+    uppercase = list(string.ascii_uppercase)  
+    digits    = list(string.digits)
+    symbols   = list(string.punctuation)
+
+    for x in range(length):
+        index = random.randint(0, len(uppercase)-1)
+        pwsd += uppercase[index]
+    
+    print(f"Heres your new password: {pwsd}")
+
+
+def passwordLength()-> int:
+    
+    try: 
+        length = int(input("Enter length of password:"))
+    except:
+        print("You need to enter a value of type 'int'!")
+        passwordLength()
+    else:
+        generatePassword(length)
+
+
+passwordLength()
